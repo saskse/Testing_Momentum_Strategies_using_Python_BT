3ded87e3408cb3229f722fd698e8ab26aa126643
Saskia Senn <113920910+saskse@users.noreply.github.com>
2023-03-09 08:37:11 +0100

    Update dir_stud.py
    final code

--- code/student/dir_stud.py
+++ code/student/dir_stud.py
@@ -1,15 +1,15 @@
-# Python library imports
+#Python library imports
 import os
 import glob
 
-# Import correction_stud.py
+#Import correction_stud.py
 import correction_stud
 
-#set path
+#Set path
 path = 'C:\\Users\\senns\\Documents\\Uni_Stuff\\2022\\Bachelorarbeit\\Final_Take\\Testing_Momentum_Strategies_using_Python_BT\\files_for_correction'
 stud_number = 0
 
-#set up directory
+#Set up directory
 for dir in os.listdir(path): 
     filenames = glob.glob(os.path.join(path + '\\' + dir + '\\2_submissions\\' , "*.xlsx"))
     stud_number +=1
